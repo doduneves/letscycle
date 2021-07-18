a4094b4304c36a095d97b71ee7caea75bad86705
eduardo.neves <eduardo.neves@intelie.com.br>
2021-07-17 21:06:14 -0300

    Removing secret key from settings.py

--- letscycle/letscycle/settings.py
+++ letscycle/letscycle/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w&^n*xk%$)e)2+pxj*m(s$n$x7+db#u05vphqu-ig*^5c$d)aq'
+# Put your secret key here
+# SECRET_KEY = '<SECRET_KEY>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
